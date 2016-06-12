a86931487eee7378b1df52b80a043a06b7366f9d
Ken <ng.ken84@gmail.com>
2016-06-12 13:57:14 -0700

    Add base template

--- main.py
+++ main.py
@@ -25,9 +25,18 @@ class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+
 class MainHandler(Handler):
     def get(self):
-        self.write("Hello, Ken!")
+        self.render("base.html");
+        
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
